d93a39ebd8e4d87911ed101d98a992a7e78ddb79
elielDev09 <elielbesayp@gmail.com>
2026-01-28 11:39:06 +0000

    cambios actuales

---
+++ models/restaurant_model.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass, asdict
+from typing import List
+
+@dataclass
+class Horario:
+    apertura: str
+    cierre: str
+
+    def to_dict(self) -> dict:
+        return asdict(self)
+
+@dataclass
+class Restaurante:
+    id: str
+    nombre: str
+    direccion: str
+    telefono: str
+    aforo_maximo: int
+    horario: Horario
+    reservas: List[dict]
+    imagen_url: str
+    imagenes: List[str]
+
+    def to_dict(self) -> dict:
+        d = asdict(self)
+        d['horario'] = self.horario.to_dict()
+        return d
